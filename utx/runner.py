9e639957d2c5cf9e78ca2fb83cd3afd691df67c2
jianbing <326333381@qq.com>
2017-12-27 11:42:37 +0800

    优化测试用例的导入

--- utx/runner.py
+++ utx/runner.py
@@ -7,13 +7,24 @@ from utx.BSTestRunner import BSTestRunner
 
 
 class TestRunner:
-    def run_test(self, path="testcase", title='接口自动化测试报告'):
+    @staticmethod
+    def run_test(*case_paths, title='接口自动化测试报告'):
+        """执行测试入口，支持导入多个文件夹的测试用例
+
+        runner = TestRunner()
+        runner.run_test(r"testcase\battle",r"testcase\chat")
+
+        :param case_paths: 测试文件夹的路径
+        :param title: 测试报告标题
+        :return:
+        """
         if not os.path.exists("report"):
             os.mkdir("report")
 
         report_dir = os.path.abspath("report")
-        suite = unittest.TestLoader().discover(path)
-
+        suite = unittest.TestSuite()
+        for case_path in case_paths:
+            suite.addTests(unittest.TestLoader().discover(case_path))
         BSTestRunner(report_dir=report_dir, title=title).run(suite)
 
         print("测试完成，请查看报告")
